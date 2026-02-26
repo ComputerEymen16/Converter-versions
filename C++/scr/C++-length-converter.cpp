87e89f28417ce3a303c20a95c34bf2b59e46b3e3
Computer_Eymen <eymenetka1616@gmail.com>
2026-02-26 16:40:38 +0300

    Add files via upload

---
+++ C++/scr/C++-length-converter.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+int main() {
+	std::cout << "Press Enter to begin." << std::endl;
+	getchar(); // Wait for the user to press Enter
+	
+	//set variables
+	double centimeter, meter, kilometer;
+	
+	//Length is received from the user in centimeters.
+	std::cout << "Enter the length in centimeters: ";
+	std::cin >> centimeter;
+	
+	//Length in centimeters is converted to meters and kilometers.
+	meter = centimeter / 100.0;
+	kilometer = centimeter / 100000.0;
+	
+	//The results are printed to the screen.
+	std::cout << "Length in meters: " << meter << " m" << std::endl;
+	std::cout << "Length in kilometers: " << kilometer << " km" << std::endl;
+
+	std::cout << "Press Enter to exit." << std::endl;
+	getchar(); // The terminal waits for the Enter key to be pressed before closing.
+	getchar();
+
+	return 0;
+}
---
+++ C++/scr_tr/C++-uzunluk-donusturucu.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+int main() {
+	std::cout << "Baslamak icin Enter tusuna basin..." << std::endl;
+	getchar(); // Enter tusuna basildi mi
+
+	//deiskenleri belirleme
+	double santimetre, metre, kilometre;
+
+	//Length is received from the user in centimeters.
+	std::cout << "Santimetre cinsinden uzunluk giriniz:  ";
+	std::cin >> santimetre;
+
+	//Metre ve kilometre santimatreye dönüştürülür.
+	metre = santimetre / 100.0;
+	kilometre = santimetre / 100000.0;
+
+	//Sonuçlar ekrana yazdırılır.
+	std::cout << "Metre cinsinden uzunluk: " << metre << " m" << std::endl;
+	std::cout << "Kilometre cinsinden uzunluk:  " << kilometre << " km" << std::endl;
+	
+	std::cout << "Kapatmak icin enter tusuna basin..." << std::endl;
+	getchar(); // terminal kapanmasi icin Enter tusuna basilmasini bekler
+	getchar();
+
+	return 0;
+}
